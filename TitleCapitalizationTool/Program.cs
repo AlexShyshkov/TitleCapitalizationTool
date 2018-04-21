2fbb8c159684a953d03b415456e01ad691fd400d
Alex Shyshkov <shi-alex@ukr.net>
2018-04-21 21:48:55 +0300

    Close#7 Rename "ch" to "symbol"

--- TitleCapitalizationTool/Program.cs
+++ TitleCapitalizationTool/Program.cs
@@ -21,9 +21,9 @@ namespace TitleCapitalizationTool
                     String[] wordsException = { "a", "an", "at", "and", "by", "but", "for", "in", "nor", "of", "on", "or", "out", "so", "to", "the", "up", "yet" };
                     for (UInt16 i = 0; i < transitionalTitle.Length; i++)
                     {
-                        Char ch = Char.ToUpper(transitionalTitle[i][0]);
+                        Char symbol = Char.ToUpper(transitionalTitle[i][0]);
                         transitionalTitle[i] = transitionalTitle[i].Remove(0, 1);
-                        transitionalTitle[i] = transitionalTitle[i].Insert(0, new string(ch, 1));
+                        transitionalTitle[i] = transitionalTitle[i].Insert(0, new string(symbol, 1));
 
                         if (i != 0 && i != transitionalTitle.Length - 1)
                         {
